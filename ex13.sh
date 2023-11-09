22ad68932aa727bf0aee223b2b7d4728ae90b38d
yassine <yassine.jemlaoui@epita.fr>
2023-11-09 22:36:31 +0100

    fix13

--- ex13.sh
+++ ex13.sh
@@ -1,6 +1,6 @@
-echo "Entrez le temps!:"
+echo "Entrez le temps! (en secondes):"
 read time
-echo "Entrez la distance en metres :" 
+echo "Entrez la distance en metres (en metres):" 
 read dist
 vitesse=$(echo "scale=2; $dist/$time" | bc)
-echo $vitesse
+echo "la vitesse est: $vitesse m/s"
